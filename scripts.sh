68aa04c59cd0d23ea63744e9c33d468347477dc8
Infsr <hannes.yuen@gmail.com>
2020-01-07 12:51:38 +0800

    add some package

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 #
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
+
+# Atmaterial Theme
+git clone https://github.com/Mrbai98/luci-theme-atmaterial package/mine/luci-theme-atmaterial
+
+# Serverchen
+git clone https://github.com/tty228/luci-app-serverchan package/mine/luci-app-serverchan
+
+# get some Lienol packages
+git clone https://github.com/Lienol/openwrt-package package/Lienol-package
+# https://github.com/Lienol/openwrt-package/issues/54
+cp -r package/Lienol-package/lienol/luci-app-fileassistant package/mine/luci-app-fileassistant
+cp -r package/Lienol-package/lienol/luci-app-filebrowser package/mine/luci-app-filebrowser
+cp -r package/Lienol-package/lienol/luci-app-passwall package/mine/luci-app-passwall
+cp -r package/Lienol-package/lienol/luci-theme-argon-mod package/mine/luci-theme-argon-mod
+#cp -r package/Lienol-package/lienol/luci-theme-bootstrap-mod package/mine/luci-theme-bootstrap-mod
+cp -r package/Lienol-package/lienol/luci-theme-netgear-mc package/mine/luci-theme-netgear-mc
+#cp package/Lienol-package/lienol/luci_my.mk package/mine
+cp -r package/Lienol-package/package/brook package/mine/brook
+cp -r package/Lienol-package/package/chinadns-ng package/mine/chinadns-ng
+cp -r package/Lienol-package/package/dns2socks package/mine/dns2socks
+cp -r package/Lienol-package/package/tcping package/mine/tcping
+rm -rf package/Lienol-package
+
+# luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr package/mine/luci-app-vssr
