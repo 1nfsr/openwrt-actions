468060e1c60a09d50f9ab4276ab51ba755ca8466
Infsr <hannes.yuen@gmail.com>
2020-01-08 14:42:49 +0800

    source update

--- scripts.sh
+++ scripts.sh
@@ -22,20 +22,23 @@ git clone https://github.com/lisaac/luci-lib-docker package/mine/luci-lib-docker
 git clone https://github.com/lisaac/luci-app-dockerman package/mine/luci-app-dockerman
 
 # get some Lienol packages
-git clone https://github.com/Lienol/openwrt-package package/Lienol-package
+git clone https://github.com/Lienol/openwrt-package package/Lienol
 # https://github.com/Lienol/openwrt-package/issues/54
-cp -r package/Lienol-package/lienol/luci-app-fileassistant package/mine/luci-app-fileassistant
-cp -r package/Lienol-package/lienol/luci-app-filebrowser package/mine/luci-app-filebrowser
-cp -r package/Lienol-package/lienol/luci-app-passwall package/mine/luci-app-passwall
-cp -r package/Lienol-package/lienol/luci-theme-argon-mod package/mine/luci-theme-argon-mod
-#cp -r package/Lienol-package/lienol/luci-theme-bootstrap-mod package/mine/luci-theme-bootstrap-mod
-cp -r package/Lienol-package/lienol/luci-theme-netgear-mc package/mine/luci-theme-netgear-mc
-#cp package/Lienol-package/lienol/luci_my.mk package/mine
-cp -r package/Lienol-package/package/brook package/mine/brook
-cp -r package/Lienol-package/package/chinadns-ng package/mine/chinadns-ng
-cp -r package/Lienol-package/package/dns2socks package/mine/dns2socks
-cp -r package/Lienol-package/package/tcping package/mine/tcping
-rm -rf package/Lienol-package
+# cp -r package/Lienol-package/lienol/luci-app-fileassistant package/mine/luci-app-fileassistant
+# cp -r package/Lienol-package/lienol/luci-app-filebrowser package/mine/luci-app-filebrowser
+# cp -r package/Lienol-package/lienol/luci-app-passwall package/mine/luci-app-passwall
+# cp -r package/Lienol-package/lienol/luci-theme-argon-mod package/mine/luci-theme-argon-mod
+# cp -r package/Lienol-package/lienol/luci-theme-bootstrap-mod package/mine/luci-theme-bootstrap-mod
+# cp -r package/Lienol-package/lienol/luci-theme-netgear-mc package/mine/luci-theme-netgear-mc
+# cp package/Lienol-package/lienol/luci_my.mk package/mine
+# cp -r package/Lienol-package/package/brook package/mine/brook
+# cp -r package/Lienol-package/package/chinadns-ng package/mine/chinadns-ng
+# cp -r package/Lienol-package/package/dns2socks package/mine/dns2socks
+# cp -r package/Lienol-package/package/tcping package/mine/tcping
+# rm -rf package/Lienol-package
 
+git clone https://github.com/coolsnowwolf/lede 
+cp -r lede/package/lean package/lean
+rm -rf lede
 # luci-app-vssr
 # git clone https://github.com/jerrykuku/luci-app-vssr package/mine/luci-app-vssr
