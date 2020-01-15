c6c67b658f64fc50faae64d9cd3db7fc5dd47908
Infsr <hannes.yuen@gmail.com>
2020-01-15 18:08:35 +0800

    little

--- scripts.sh
+++ scripts.sh
@@ -6,7 +6,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
-# atmaterial主题
+# atmaterial theme
 git clone https://github.com/Mrbai98/luci-theme-atmaterial package/mine/luci-theme-atmaterial
 
 # Serverchan
