1bbdc7fb060a58c613e9cb603de0a19a4f6c504f
Infsr <i@infsr.me>
2020-04-02 12:40:22 +0800

    update

--- scripts.sh
+++ scripts.sh
@@ -7,19 +7,27 @@
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
 # atmaterial theme
-git clone https://github.com/Mrbai98/luci-theme-atmaterial package/mine/luci-theme-atmaterial
+git clone https://github.com/Mrbai98/luci-theme-atmaterial package/own-apps/luci-theme-atmaterial
 
 # Serverchan
-git clone https://github.com/tty228/luci-app-serverchan package/mine/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan package/own-apps/luci-app-serverchan
 
 # KoolProxy
 # https://github.com/openwrt-develop/luci-app-koolproxy package/mine/luci-app-koolproxy
 
 # Lienol-package
 git clone https://github.com/Lienol/openwrt-package package/Lienol-package
-cp -r package/Lienol-package/lienol/luci-app-passwall package/mine/luci-app-passwall
-cp -r package/Lienol-package/package/brook package/mine/brook
-cp -r package/Lienol-package/package/chinadns-ng package/mine/chinadns-ng
+cp -r package/Lienol-package/lienol/luci-app-passwall package/own-apps/luci-app-passwall
+cp -r package/Lienol-package/package/brook package/own-apps/brook
+cp -r package/Lienol-package/package/chinadns-ng package/own-apps/chinadns-ng
+
+# SmartDNS
+git clone https://github.com/pymumu/luci-app-smartdns.git package/own-apps/luci-app-smartdns
+git clone https://github.com/pymumu/smartdns.git package/own-apps/smartdns
+
+# 
+git clone https://github.com/vernesong/OpenClash.git package/own-apps/OpenClash
+mv package/own-apps/OpenClash/luci-app-openclash ../
 cp -r package/Lienol-package/package/dns2socks package/mine/dns2socks
 cp -r package/Lienol-package/package/tcping package/mine/tcping
 rm -rf package/Lienol-package
