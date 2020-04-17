b4b270abbb036fc2f5c06a73318c8d1c73d5cd40
Infsr <i@infsr.me>
2020-04-18 03:53:38 +0800

    Revert

--- scripts.sh
+++ scripts.sh
@@ -6,9 +6,3 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
-# Atmaterial Theme
-git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/My_Apps/luci-theme-atmaterial
-
-# ServerChen App
-git clone https://github.com/tty228/luci-app-serverchan.git package/My_Apps/luci-app-serverchan
-
