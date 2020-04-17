8a29b183aa217d0a12bbacbb0545fbcdc1f4720f
Infsr <i@infsr.me>
2020-04-17 12:14:38 +0800

    Pure OpenWrt

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 OpenWrt Continuous Integration
 ==============================
 
-OpenWrt Source: [OpenWrt](https://github.com/coolsnowwolf/lede)
-
-Package Source: [Package](https://github.com/Lienol/openwrt-package)
-
-Other Source: [Atmaterial Theme](https://github.com/Mrbai98/luci-theme-atmaterial)
-
-Other Source: [Serverchan](https://github.com/tty228/luci-app-serverchan)
-
-Other Source: [DiskMan](https://github.com/lisaac/luci-app-diskman)
-
-Other Source: [DockerManager](https://github.com/lisaac/luci-app-dockerman)
+[Source]
--- scripts.sh
+++ scripts.sh
@@ -6,20 +6,3 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
-# atmaterial theme
-#git clone https://github.com/Mrbai98/luci-theme-atmaterial package/own-apps/luci-theme-atmaterial
-
-# Serverchan
-#git clone https://github.com/tty228/luci-app-serverchan package/own-apps/luci-app-serverchan
-
-# KoolProxy
-# https://github.com/openwrt-develop/luci-app-koolproxy package/mine/luci-app-koolproxy
-
-# Lienol-package
-#git clone https://github.com/Lienol/openwrt-package package/Lienol-package
-#cp -r package/Lienol-package/lienol/luci-app-passwall package/own-apps/luci-app-passwall
-#cp -r package/Lienol-package/package/brook package/own-apps/brook
-#cp -r package/Lienol-package/package/chinadns-ng package/own-apps/chinadns-ng
-#cp -r package/Lienol-package/package/dns2socks package/own-apps/dns2socks
-#cp -r package/Lienol-package/package/tcping package/own-apps/tcping
-#rm -rf package/Lienol-package
