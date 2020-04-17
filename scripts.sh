714e34500072057194c43f7e7f48ffeb901e4185
Infsr <i@infsr.me>
2020-04-17 16:46:11 +0800

    Add DiskMan

--- scripts.sh
+++ scripts.sh
@@ -3,6 +3,12 @@
 #+    custom configuration    +#   
 #------------------------------#
 
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
+
+# DiskMan
+git clone https://github.com/lisaac/luci-app-diskman.git package/My_Apps/luci-app-diskman
+mkdir package/My_Apps/Parted
+mv package/My_Apps/luci-app-diskman/Parted.Makefile package/My_Apps/Parted/Makefile
 
