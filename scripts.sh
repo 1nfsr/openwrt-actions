22b7d4565af22a1098daf7f7074ccfde73803440
Infsr <i@infsr.me>
2020-04-17 23:54:38 +0800

    Revert to previous version&&Add Atmaterial&&ServerChen

--- scripts.sh
+++ scripts.sh
@@ -6,13 +6,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
-# Fullconenat
-git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
-# Download fullconenat.patch to package/network/config/firewall/patches/
-mkdir package/network/config/firewall/patches
-wget -P package/network/config/firewall/patches/ https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/fullconenat.patch
-# Patch LuCI
-pushd feeds/luci
-wget -O- https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/luci.patch | git apply
-popd
+# Atmaterial Theme
+git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/My_Apps/luci-theme-atmaterial
+
+# ServerChen App
+git clone https://github.com/tty228/luci-app-serverchan.git package/My_Apps/luci-app-serverchan
 
