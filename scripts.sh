e02f1d4b4f1fa24af158c694b28e3104994da23c
Infsr <i@infsr.me>
2020-04-18 13:22:03 +0800

    actions

--- scripts.sh
+++ scripts.sh
@@ -6,3 +6,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
+# SSR PLUS+
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
