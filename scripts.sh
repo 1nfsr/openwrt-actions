e360dd1c0d0688e81cf8657bf692d6b2ff10e3a6
Infsr <i@infsr.me>
2020-04-20 01:16:07 +0800

    update

--- scripts.sh
+++ scripts.sh
@@ -7,17 +7,17 @@
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
 # SSR PLUS+
-mkdir package/base-files/files/config
-echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+#mkdir package/base-files/files/config
+#echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+#echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+#./scripts/feeds update -a && ./scripts/feeds install -a
 
 # VSSR
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
 # ServerChen
-#git clone https://github.com/tty228/luci-app-serverchan.git package/OpenWrtApps/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/OpenWrtApps/luci-app-serverchan
 
 # Atmaterial
-#git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/OpenWrtApps/luci-theme-atmaterial
+git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/OpenWrtApps/luci-theme-atmaterial
