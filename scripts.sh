49f13429596acb046d45659175c2bae7eae84d03
Infsr <i@infsr.me>
2020-04-19 15:36:01 +0800

    rm{btrfs-progs}&&add{ServerChen,Atmaterial}

--- scripts.sh
+++ scripts.sh
@@ -11,3 +11,9 @@ mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
+
+# ServerChen
+git clone https://github.com/tty228/luci-app-serverchan package/OpenWrtApps/luci-app-serverchan
+
+# Atmaterial
+git clone https://github.com/Mrbai98/luci-theme-atmaterial package/OpenWrtApps/luci-theme-atmaterial
