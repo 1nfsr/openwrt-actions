b2d2807f40dbb775a161e955401592dc64e752ab
Infsr <i@infsr.me>
2020-10-22 23:17:37 +0800

    upgrade

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,6 +7,61 @@ sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_genera
 mkdir package/lean
 cp -r $GITHUB_WORKSPACE/default-settings package/lean/
 
+# Community packages
+mkdir package/community
+pushd package/community
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+rm -rf openwrt-package/lienol/luci-app-ssr-python-pro-server
+# Add ServerChan.
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+# Add luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
+# Add luci-app-jd-dailybonus
+git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
+# Add luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+# luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker
+
+popd
+
+# Mod zzz-default-settings
+pushd package/lean/default-settings/files
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" zzz-default-settings
+popd
+
+# Remove orig kcptun
+rm -rf ./feeds/packages/net/kcptun
+# Max connections
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+# Add po2lmo
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
+
+# Use Lean's golang to fix latest v2ray compile errors
+pushd feeds/packages/lang
+rm -rf golang
+svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang
+popd
+
+# Convert Translation
+cp $GITHUB_WORKSPACE/scripts/convert-translation.sh .
+chmod +x ./convert-translation.sh
+./convert-translation.sh || true
+
+# Remove upx
+cp $GITHUB_WORKSPACE/scripts/remove-upx.sh .
+chmod +x ./remove-upx.sh
+./remove-upx.sh || true
+
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
     echo 'CONFIG_KERNEL_BUILD_USER="Infsr"' >>.config ||
