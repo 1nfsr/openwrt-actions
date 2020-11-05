a463d07741ffa61bc7bb793437adb3331fe970b2
Infsr <i@infsr.me>
2020-11-05 22:02:33 +0800

    update

--- scripts/serverchan.sh
+++ scripts/serverchan.sh
@@ -2,7 +2,7 @@
 
 if [ steps.compile.outputs.status == 'success' ]
 then
-    curl https://sc.ftqq.com/${SCKEY}.send?text=🎉OpenWrt编译完成😋
+    curl https://sc.ftqq.com/${SCKEY}.send?text=OpenWrt编译完成
 else
-    curl https://sc.ftqq.com/${SCKEY}.send?text=❌OpenWrt编译失败😂
+    curl https://sc.ftqq.com/${SCKEY}.send?text=OpenWrt编译失败
 fi
