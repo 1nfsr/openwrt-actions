02d5b4acbfc81b4836192b9b368aa9e24ffd85e2
Infsr <i@infsr.me>
2020-11-05 08:37:07 +0800

    update

---
+++ scripts/serverchan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ steps.compile.outputs.status == 'success' ]
+then
+    curl https://sc.ftqq.com/${{ secrets.SCKEY }}.send?text="🎉OpenWrt_${{ env.date3 }}_${{matrix.target}}编译完成😋"
+else
+    curl https://sc.ftqq.com/${{ secrets.SCKEY }}.send?text="❌OpenWrt_${{ env.date3 }}_${{matrix.target}}编译失败😂"
+fi
