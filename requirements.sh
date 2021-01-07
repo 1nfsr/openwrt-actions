e39df5becbedac7dc5cf98623fd2590f5206b82b
Infsr <i@infsr.me>
2021-01-08 01:33:21 +0800

    update

--- requirements.sh
+++ requirements.sh
@@ -6,7 +6,7 @@
 
 sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
 sudo -E apt-get -qq update
-sudo -E apt-get -qq install gcc binutils bzip2 flex python3 perl make find grep diff unzip gawk getopt subversion libz-dev libc-dev rsync
+sudo -E apt-get -qq install gcc binutils bzip2 flex python3 python3-dev perl make find grep diff unzip gawk getopt subversion libz-dev libc-dev rsync
 sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean
 sudo timedatectl set-timezone "Asia/Shanghai"
