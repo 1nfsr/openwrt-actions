ae73c31c027cd0c79f245790d756c4a200223494
Infsr <i@infsr.me>
2021-01-07 19:08:27 +0800

    update

--- extra.sh
+++ extra.sh
@@ -10,7 +10,11 @@ sed -i 's/OpenWrt/x86/g' package/base-files/files/bin/config_generate
 # 
 sed -i "s/'UTC'/'CST-8'\n                set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
 sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION='18.06'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION='19.07'" >> package/base-files/files/etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='Infsr build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt '" >> package/base-files/files/etc/openwrt_release
+
+git clone https://github.com/garypang13/openwrt-packages package/extra-packages
