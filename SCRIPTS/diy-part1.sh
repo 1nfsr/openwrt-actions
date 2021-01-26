3787a9cee6f865548c8abd97ecd1afdc07b9d6cd
Infsr <i@infsr.me>
2021-01-27 07:39:24 +0800

    [+] 更新配置

--- SCRIPTS/diy-part1.sh
+++ SCRIPTS/diy-part1.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+#移除SNAPSHOT标签
+sed -i 's,SNAPSHOT,,g' include/version.mk
+sed -i 's,snapshots,,g' package/base-files/image-config.in
+sed -i 's/ %V,//g' package/base-files/files/etc/banner
+
+#使用O2级别的优化
+sed -i 's/Os/O2/g' include/target.mk
+sed -i 's/O2/O2/g' ./rules.mk
+
+#修改最大连接数
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+
+#自定义编译信息
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+sed -i "$ a\DISTRIB_DESCRIPTION='Built by Infsr($(date +%Y.%m.%d))@%D %V %C'" package/base-files/files/etc/openwrt_release
+sed -i '/%D/a\ Infsr Build' package/base-files/files/etc/banner
