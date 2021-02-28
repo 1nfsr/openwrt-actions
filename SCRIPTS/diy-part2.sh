7e77b016088092ed24b6165e2129a53022029958
Infsr <i@infsr.me>
2021-03-01 00:17:36 +0800

    revert bash version

--- SCRIPTS/diy-part2.sh
+++ SCRIPTS/diy-part2.sh
@@ -110,3 +110,7 @@ echo '
 CONFIG_CRYPTO_CHACHA20_X86_64=y
 CONFIG_CRYPTO_POLY1305_X86_64=y
 ' >> ./target/linux/x86/64/config-5.4
+
+#fix
+rm -rf feeds/packages/utils/bash
+svn co https://github.com/coolsnowwolf/packages/trunk/utils/bash feeds/packages/utils/bash
