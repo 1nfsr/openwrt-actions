4d58e84c9e0693d5364755ce958d046152b588da
Infsr <i@infsr.me>
2021-02-28 13:42:56 +0800

    fix

--- SCRIPTS/diy-part2.sh
+++ SCRIPTS/diy-part2.sh
@@ -21,7 +21,7 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/ap
 
 # 添加 openclash
 #svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/apps/luci-app-openclash
-cp -rf ${GITHUB_WORKSPACE}/apps/luci-app-openclash package/apps/luci-app-openclash
+mv ${GITHUB_WORKSPACE}/apps/luci-app-openclash package/apps/
 
 # 添加 clash ui面板
 mkdir -p package/base-files/files/www
