785672c3e05e729adc9cfc73fe32326e0eab7285
Infsr <i@infsr.me>
2021-03-05 01:06:12 +0800

    custom AFP Netatalk (Apple Time Machine)

--- install-scripts/custom.sh
+++ install-scripts/custom.sh
@@ -13,3 +13,15 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
 sed -i "$ a\DISTRIB_DESCRIPTION='Built by Infsr($(date +%Y.%m.%d))@%D %V %C'" package/base-files/files/etc/openwrt_release
 sed -i '/%D/a\ Infsr Build' package/base-files/files/etc/banner
+
+# AFP Netatalk share configuration (aka Apple Time Machine)
+# reference: https://openwrt.org/docs/guide-user/services/nas/netatalk_configuration
+# add user
+echo "infsr::0:0:99999:7:::" >> package/base-files/files/etc/shadow
+sed -i 's/infsr::0:0:99999:7:::/infsr:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:18678:0:99999:7:::/g' package/base-files/files/etc/shadow
+echo "infsr:x:1000:1000::/home/infsr:/usr/bin/zsh" >> package/base-files/files/etc/passwd
+sed -i 's/100:/100:infsr/g' package/base-files/files/etc/group
+# Modify services
+mkdir -p package/base-files/files/etc/avahi
+mv ${GITHUB_WORKSPACE}/base-files/etc/avahi/* package/base-files/files/etc/avahi/
+mv ${GITHUB_WORKSPACE}/base-files/etc/{afp.conf,extmap.conf} package/base-files/files/etc/
