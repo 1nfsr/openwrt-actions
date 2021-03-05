a59562c083ecdaf344b9328e2594ea57918a3f6f
Infsr <i@infsr.me>
2021-03-05 13:20:53 +0800

    turboacc

--- apps-scripts/custom.sh
+++ apps-scripts/custom.sh
@@ -9,3 +9,17 @@ mv ${GITHUB_WORKSPACE}/apps/miniupnpd feeds/packages/net/
 
 ## Docker
 sed -i 's/+docker/+docker +dockerd/g' feeds/luci/applications/luci-app-dockerman/Makefile
+
+
+## Turboacc
+mv ${GITHUB_WORKSPACE}/apps/luci-app-turboacc package/apps/
+# Fix DNS port
+rm -rf package/apps/luci-app-turboacc/root/etc/init.d/turboacc
+mv $GITHUB_WORKSPACE/apps-custom-files/turboacc/root/etc/init.d/turboacc package/apps/luci-app-turboacc/root/etc/init.d/
+# Patch firewall support fullconenat
+mkdir package/network/config/firewall/patches
+wget -O package/network/config/firewall/patches/fullconenat.patch https://github.com/coolsnowwolf/lede/raw/master/package/network/config/firewall/patches/fullconenat.patch
+# Patch remove firewall view offload
+mv $GITHUB_WORKSPACE/PATCH/remove_firewall_view_offload.patch ./
+patch -p1 < remove_firewall_view_offload.patch
+rm -rf remove_firewall_view_offload.patch
