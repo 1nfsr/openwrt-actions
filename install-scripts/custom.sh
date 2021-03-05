c2c7f71cffeeb780c9eebd664801fdc0e895dc92
Infsr <i@infsr.me>
2021-03-05 12:48:51 +0800

    modified format

--- install-scripts/custom.sh
+++ install-scripts/custom.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 
-# TCP Fast Open(TFO)
+
+## TCP Fast Open(TFO)
 # reference 1: https://wiki.archlinux.org/index.php/sysctl
 # reference 2: https://www.keycdn.com/support/tcp-fast-open
 mv ${GITHUB_WORKSPACE}/base-files/etc/sysctl.d/60_tcp_fastopen.conf package/base-files/files/etc/sysctl.d/
 
-# Modify default Shell
+
+## Modify default Shell
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
-# oh-my-zsh
+
+## oh-my-zsh
 mkdir -p package/base-files/files/etc/ohmyzsh
 mkdir -p package/base-files/files/home/infsr
 mv ${GITHUB_WORKSPACE}/base-files/etc/ohmyzsh package/base-files/files/etc/
 mv ${GITHUB_WORKSPACE}/base-files/root/.zshrc package/base-files/files/root/
 mv ${GITHUB_WORKSPACE}/base-files/home/infsr/.zshrc package/base-files/files/home/infsr/
 
-# custom compile information
+
+## custom compile information
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
 sed -i "$ a\DISTRIB_DESCRIPTION='Built by Infsr($(date +%Y.%m.%d))@%D %V %C'" package/base-files/files/etc/openwrt_release
 sed -i '/%D/a\ Infsr Build' package/base-files/files/etc/banner
 
-# AFP Netatalk share configuration (aka Apple Time Machine)
+
+## AFP Netatalk share configuration (aka Apple Time Machine)
 # reference: https://openwrt.org/docs/guide-user/services/nas/netatalk_configuration
 # add user
 echo "infsr::0:0:99999:7:::" >> package/base-files/files/etc/shadow
@@ -33,22 +38,26 @@ mkdir -p package/base-files/files/etc/avahi
 mv ${GITHUB_WORKSPACE}/base-files/etc/avahi/* package/base-files/files/etc/avahi/
 mv ${GITHUB_WORKSPACE}/base-files/etc/{afp.conf,extmap.conf} package/base-files/files/etc/
 
-# OpenWrt with Nginx
+
+## OpenWrt with Nginx
 # reference: https://openwrt.org/docs/guide-user/services/webserver/nginx
 # add certificate
 mkdir -p package/base-files/files/etc/nginx
 mv ${GITHUB_WORKSPACE}/base-files/etc/nginx/conf.d package/base-files/files/etc/nginx/
 
-# SSH (root) Public key authentication
+
+## SSH (root) Public key authentication
 mkdir -p package/base-files/files/etc/ssh
 mv ${GITHUB_WORKSPACE}/base-files/etc/ssh/sshd_config package/base-files/files/etc/ssh/
 # add ssh-keygen !!!Notice: replace with your own key
 mkdir -p package/base-files/files/root/.ssh
 mv ${GITHUB_WORKSPACE}/base-files/root/.ssh/* package/base-files/files/root/.ssh/
 
-# Realtek RTL8168 Driver for Openwrt
+
+## Realtek RTL8168 Driver for Openwrt
 # sources: https://github.com/BROBIRD/openwrt-r8168
 git clone --depth=1 https://github.com/BROBIRD/openwrt-r8168.git  package/apps/openwrt-r8168
 
-# Remove status show model
+
+## Remove status show model
 sed -i '55d' feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
--- install-scripts/normal.sh
+++ install-scripts/normal.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
 
-# remove show SNAPSHOT tags
+
+## remove show SNAPSHOT tags
 sed -i 's,SNAPSHOT,,g' include/version.mk
 sed -i 's,snapshots,,g' package/base-files/image-config.in
 sed -i 's/ %V,//g' package/base-files/files/etc/banner
 
-# use O2 optimization
+
+## use O2 optimization
 sed -i 's/Os/O2/g' include/target.mk
 sed -i 's/O2/O2/g' ./rules.mk
 
-# Modify default IP
+
+## Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
 
-# Modify default hostname
+
+## Modify default hostname
 sed -i "s/OpenWrt/x86/g" package/base-files/files/bin/config_generate
 
-# Modify Time zone
+
+## Modify Time zone
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-# Modify default password (password)
+
+## Modify default password (password)
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# Modify the maximum number of Active Connections
+
+## Modify the maximum number of Active Connections
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
-# Crypto algorithm support kernel-5.4
+
+## Crypto algorithm support kernel-5.4
 # reference 1: https://en.wikipedia.org/wiki/Salsa20
 # reference 2: https://tools.ietf.org/html/rfc7539
 echo '
