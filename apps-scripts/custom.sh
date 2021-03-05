a620d2f3a381a65431cbbfd35e68bf39a1801b09
Infsr <i@infsr.me>
2021-03-05 13:08:32 +0800

    docker service

--- apps-scripts/custom.sh
+++ apps-scripts/custom.sh
@@ -5,3 +5,7 @@
 ## Revert miniupnpd version(2.0)
 rm -Rf feeds/packages/net/miniupnpd
 mv ${GITHUB_WORKSPACE}/apps/miniupnpd feeds/packages/net/
+
+
+## Docker
+sed -i 's/+docker/+docker +dockerd/g' feeds/luci/applications/luci-app-dockerman/Makefile
