6278b4bfb8fd20f0be2d71894bec25a3bbaa2867
Infsr <i@infsr.me>
2021-04-01 06:43:29 +0800

    docker config

--- apps-scripts/custom.sh
+++ apps-scripts/custom.sh
@@ -10,7 +10,7 @@ mv ${GITHUB_WORKSPACE}/apps/miniupnpd feeds/packages/net/
 ## Docker
 sed -i 's/+docker/+docker +dockerd +docker-compose/g' feeds/luci/applications/luci-app-dockerman/Makefile
 rm -Rf feeds/packages/utils/dockerd/files/etc/config/dockerd
-mv ${GITHUB_WORKSPACE}/apps/dockerd/etc/config/dockerd feeds/packages/utils/dockerd/files/etc/config/
+mv ${GITHUB_WORKSPACE}/apps/docker/etc/config/dockerd feeds/packages/utils/dockerd/files/etc/config/
 
 
 ## Turboacc
