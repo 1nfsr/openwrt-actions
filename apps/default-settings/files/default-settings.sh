9f2eb412b456d22776639d0a06d05c1bb38cb4e3
Infsr <i@infsr.me>
2021-04-14 14:21:53 +0800

    update .config

--- apps/default-settings/files/default-settings.sh
+++ apps/default-settings/files/default-settings.sh
@@ -13,7 +13,6 @@ uci commit fstab
 ln -sf /sbin/ip /usr/bin/ip
 
 sed -i 's#https://downloads.openwrt.org#https://mirrors.cloud.tencent.com/lede#g' /etc/opkg/distfeeds.conf
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
 sed -i '/REDIRECT --to-ports 53/d' /etc/firewall.user
 echo "iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53" >> /etc/firewall.user
@@ -21,7 +20,7 @@ echo "iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53"
 
 
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-echo "DISTRIB_REVISION='R21.3.27'" >> /etc/openwrt_release
+echo "DISTRIB_REVISION=' '" >> /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
 
@@ -34,5 +33,7 @@ echo 'hsts=0' > /root/.wgetrc
 
 rm -rf /tmp/luci-modulecache/
 rm -f /tmp/luci-indexcache
+
+/etc/init.d/dockerd restart
 
 exit 0
