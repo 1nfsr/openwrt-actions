660552d1c053f83cf4d22e33eab43b0a10b8be06
Infsr <i@infsr.me>
2021-05-08 11:44:39 +0800

    optimization files

--- scripts/DLC.sh
+++ scripts/DLC.sh
@@ -2,3 +2,27 @@
 
 
 cp -r ${GITHUB_WORKSPACE}/DLC package/
+
+
+## Adguardhome
+if [ `grep -c 'luci-app-adguardhome=y' .config` -ne '0' ]; then
+	echo "Adguardhome configuration complete!"
+else
+	echo "Adguardhome is not set yet"
+fi
+
+
+## Openclash
+if [ `grep -c 'luci-app-openclash=y' .config` -ne '0' ]; then
+	echo "Openclash configuration complete!"
+else
+	echo "Openclash is not set yet"
+fi
+
+
+## Turboacc
+if [ `grep -c 'luci-app-turboacc=y' .config` -ne '0' ]; then
+	echo "Turboacc configuration complete!"
+else
+	echo "Turboacc is not set yet"
+fi
--- scripts/Modification.sh
+++ scripts/Modification.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 
+## Docker
+if [ `grep -c 'luci-app-dockerman=y' .config` -ne '0' ]; then
+	echo "Docker configuration complete!"
+else
+	echo "Docker is not set yet"
+fi
+
+
 ## Nginx
-mkdir -p package/base-files/files/etc/nginx
-cp -rf ${GITHUB_WORKSPACE}/Modification/nginx/conf.d package/base-files/files/etc/nginx/
-mkdir -p package/base-files/files/etc/uwsgi/vassals
-cp -rf ${GITHUB_WORKSPACE}/Modification/uwsgi/vassals/mysite.ini package/base-files/files/etc/uwsgi/vassals/
-# custom config
-sed -i 's/true/false/g' feeds/packages/net/nginx-util/files/nginx.config
-cp -rf ${GITHUB_WORKSPACE}/Modification/nginx/nginx.conf package/base-files/files/etc/nginx/
+if [ `grep -c 'nginx-mod-luci=y' .config` -ne '0' ]; then
+	mkdir -p package/base-files/files/etc/nginx
+	cp -rf ${GITHUB_WORKSPACE}/Modification/nginx/conf.d package/base-files/files/etc/nginx/
+	mkdir -p package/base-files/files/etc/uwsgi/vassals
+	cp -rf ${GITHUB_WORKSPACE}/Modification/uwsgi/vassals/mysite.ini package/base-files/files/etc/uwsgi/vassals/
+	# custom config
+	sed -i 's/true/false/g' feeds/packages/net/nginx-util/files/nginx.config
+	cp -rf ${GITHUB_WORKSPACE}/Modification/nginx/nginx.conf package/base-files/files/etc/nginx/
+	echo "Nginx configuration complete!"
+else
+	echo "Nginx is not set yet"
+fi
 
 
 ## SmartDNS
-if [ `grep -c 'luci-app-smartdns=y' config` -ne '0' ]; then
+if [ `grep -c 'luci-app-smartdns=y' .config` -ne '0' ]; then
 	mkdir -p feeds/packages/net/smartdns/files/etc/config/
 	cp -rf ${GITHUB_WORKSPACE}/Modification/smartdns/config/smartdns feeds/packages/net/smartdns/files/etc/config/
 	mkdir -p feeds/packages/net/smartdns/files/package/openwrt/files/etc/init.d/
@@ -21,6 +34,7 @@ if [ `grep -c 'luci-app-smartdns=y' config` -ne '0' ]; then
 	# disable Rebind protection&&RBL checking and similar services
 	sed -i "s/option rebind_protection 1/option rebind_protection 0/g" package/network/services/dnsmasq/files/dhcp.conf
 	sed -i "s/option rebind_localhost 1/option rebind_localhost 0/g" package/network/services/dnsmasq/files/dhcp.conf
+	echo "SmartDNS configuration complete!"
 else
 	echo "SmartDNS is not set yet"
 fi
