b88bb8869a2a8138589c7878421dbc439c19563a
Infsr <i@infsr.me>
2021-05-08 20:57:56 +0800

    fix smartdns

--- scripts/Modification.sh
+++ scripts/Modification.sh
@@ -28,9 +28,9 @@ fi
 if [ `grep -c 'luci-app-smartdns=y' .config` -ne '0' ]; then
 	#mkdir -p feeds/packages/net/smartdns/files/etc/config/
 	#cp -rf ${GITHUB_WORKSPACE}/Modification/smartdns/config/smartdns feeds/packages/net/smartdns/files/etc/config/
-	mkdir -p feeds/packages/net/smartdns/files/package/openwrt/files/etc/init.d/
-	mv ${GITHUB_WORKSPACE}/Modification/smartdns/init.d/smartdnsprocd feeds/packages/net/smartdns/files/package/openwrt/files/etc/init.d/
-	sed -i '/init.d\/smartdns/a\    $(INSTALL_BIN) .\/files\/package\/openwrt\/files\/etc\/init.d\/smartdnsprocd $(1)\/etc\/init.d\/smartdnsprocd' feeds/packages/net/smartdns/Makefile
+	#mkdir -p feeds/packages/net/smartdns/files/package/openwrt/files/etc/init.d/
+	#mv ${GITHUB_WORKSPACE}/Modification/smartdns/init.d/smartdnsprocd feeds/packages/net/smartdns/files/package/openwrt/files/etc/init.d/
+	#sed -i '/init.d\/smartdns/a\    $(INSTALL_BIN) .\/files\/package\/openwrt\/files\/etc\/init.d\/smartdnsprocd $(1)\/etc\/init.d\/smartdnsprocd' feeds/packages/net/smartdns/Makefile
 	# disable Rebind protection&&RBL checking and similar services
 	sed -i "s/option rebind_protection 1/option rebind_protection 0/g" package/network/services/dnsmasq/files/dhcp.conf
 	sed -i "s/option rebind_localhost 1/option rebind_localhost 0/g" package/network/services/dnsmasq/files/dhcp.conf
