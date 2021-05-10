5af9aa01f437102d72ad8d0f50ae3ee627c821ee
Infsr <i@infsr.me>
2021-05-10 18:49:37 +0800

    add docker

--- scripts/Modification.sh
+++ scripts/Modification.sh
@@ -3,26 +3,32 @@
 
 ## AFP Netatalk share configuration (aka Apple Time Machine)
 # reference: https://openwrt.org/docs/guide-user/services/nas/netatalk_configuration
-# add user
-echo "infsr::0:0:99999:7:::" >> package/base-files/files/etc/shadow
-sed -i 's/infsr::0:0:99999:7:::/infsr:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:18678:0:99999:7:::/g' package/base-files/files/etc/shadow
-echo "infsr:x:1000:1000::/home/infsr:/usr/bin/zsh" >> package/base-files/files/etc/passwd
-sed -i 's/100:/100:infsr/g' package/base-files/files/etc/group
-# Modify services
-mkdir -p package/base-files/files/etc/avahi
-sed -i 's/#host-name=foo/host-name=Time Capsule/g' feeds/packages/libs/avahi/files/avahi-daemon.conf
-sed -i '/#domain-name/a\enable-dbus=no' feeds/packages/libs/avahi/files/avahi-daemon.conf
-mv -f ${GITHUB_WORKSPACE}/Modification/netatalk/Makefile feeds/packages/net/netatalk/
-mv -f ${GITHUB_WORKSPACE}/Modification/netatalk/files/afp.conf feeds/packages/net/netatalk/files/
-mv -f ${GITHUB_WORKSPACE}/Modification/avahi/Makefile feeds/packages/libs/avahi/
-mv -f ${GITHUB_WORKSPACE}/Modification/avahi/files/service-afp feeds/packages/libs/avahi/files/
-# Generate a version UUID
-uuidv4=$(wget https://www.uuidgenerator.net/api/version4 -q -O -)
-sed -i "s/uuid/${uuidv4}/g" feeds/packages/libs/avahi/files/service-afp
+if [ `grep -c 'CONFIG_PACKAGE_netatalk=y' .config` -ne '0' ]; then
+	# add user
+	echo "infsr::0:0:99999:7:::" >> package/base-files/files/etc/shadow
+	sed -i 's/infsr::0:0:99999:7:::/infsr:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:18678:0:99999:7:::/g' package/base-files/files/etc/shadow
+	echo "infsr:x:1000:1000::/home/infsr:/usr/bin/zsh" >> package/base-files/files/etc/passwd
+	sed -i 's/100:/100:infsr/g' package/base-files/files/etc/group
+	# Modify services
+	mkdir -p package/base-files/files/etc/avahi
+	sed -i 's/#host-name=foo/host-name=Time Capsule/g' feeds/packages/libs/avahi/files/avahi-daemon.conf
+	sed -i '/#domain-name/a\enable-dbus=no' feeds/packages/libs/avahi/files/avahi-daemon.conf
+	mv -f ${GITHUB_WORKSPACE}/Modification/netatalk/Makefile feeds/packages/net/netatalk/
+	mv -f ${GITHUB_WORKSPACE}/Modification/netatalk/files/afp.conf feeds/packages/net/netatalk/files/
+	mv -f ${GITHUB_WORKSPACE}/Modification/avahi/Makefile feeds/packages/libs/avahi/
+	mv -f ${GITHUB_WORKSPACE}/Modification/avahi/files/service-afp feeds/packages/libs/avahi/files/
+	# Generate a version UUID
+	uuidv4=$(wget https://www.uuidgenerator.net/api/version4 -q -O -)
+	sed -i "s/uuid/${uuidv4}/g" feeds/packages/libs/avahi/files/service-afp
+	echo "AFP configuration complete!"
+else
+	echo "AFP is not set yet"
+fi
 
 
 ## Docker
 if [ `grep -c 'luci-app-dockerman=y' .config` -ne '0' ]; then
+	sed -i 's/+docker/+docker +dockerd +docker-compose/g' feeds/luci/applications/luci-app-dockerman/Makefile
 	echo "Docker configuration complete!"
 else
 	echo "Docker is not set yet"
@@ -32,6 +38,10 @@ fi
 ## Mount points
 mkdir -p package/base-files/files/etc/config/
 cp -rf ${GITHUB_WORKSPACE}/Modification/base-files/etc/config/fstab package/base-files/files/etc/config/
+
+
+## Remove status show model
+sed -i '55d' feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
 
 ## TCP Fast Open(TFO)
