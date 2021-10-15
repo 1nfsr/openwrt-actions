b4873af54ff0ab5e488773a3f59ac86382eb22ca
Infsr <i@infsr.me>
2021-10-15 10:37:36 +0800

    remove patchs

--- scripts/DLC.sh
+++ scripts/DLC.sh
@@ -15,8 +15,8 @@ if [ `grep -c 'luci-app-turboacc=y' .config` -ne '0' ]; then
     cp -rf ${GITHUB_WORKSPACE}/Modification/turboacc/patch/fullconenat.patch package/network/config/firewall/patches/
     patch -p1 < ${GITHUB_WORKSPACE}/Modification/turboacc/patch/001-remove_firewall_view_offload.patch
     # Fix conntrack events patch netfilter
-    cp -rf ${GITHUB_WORKSPACE}/Modification/turboacc/patch/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.4/
-    cp -rf ${GITHUB_WORKSPACE}/Modification/turboacc/patch/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.10/
+    #cp -rf ${GITHUB_WORKSPACE}/Modification/turboacc/patch/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.4/
+    #cp -rf ${GITHUB_WORKSPACE}/Modification/turboacc/patch/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.10/
     cp -rf ${GITHUB_WORKSPACE}/Modification/turboacc/patch/953-net-patch-linux-kernel-to-support-shortcut-fe.patch target/linux/generic/hack-5.10/
     # Implementation of RFC3489-compatible full cone SNAT
     echo "iptables -t nat -A POSTROUTING -o eth1 -j FULLCONENAT" >> package/network/config/firewall/files/firewall.user
