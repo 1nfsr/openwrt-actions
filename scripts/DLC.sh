5f4e55e564344a7e7b10ebe587da43f4915d125b
Infsr <i@infsr.me>
2021-10-16 16:54:21 +0800

    update config

--- scripts/DLC.sh
+++ scripts/DLC.sh
@@ -7,6 +7,8 @@ cp -rf ${GITHUB_WORKSPACE}/Applications/luci-app-openclash package/custom/luci-a
 bash ${GITHUB_WORKSPACE}/Modification/openclash/get_clash_core.sh amd64
 
 # dnsmasq forwarder
-sed -i '/option resolvfile/i\      option noresolv         0' package/network/services/dnsmasq/files/dhcp.conf
-sed -i 's/option resolvfile/#option resolvfile/g' package/network/services/dnsmasq/files/dhcp.conf
-sed -i "/#list server/i\    list server             '127.0.0.1#7874'" package/network/services/dnsmasq/files/dhcp.conf
+#sed -i '/option resolvfile/i\      option noresolv         0' package/network/services/dnsmasq/files/dhcp.conf
+#sed -i 's/option resolvfile/#option resolvfile/g' package/network/services/dnsmasq/files/dhcp.conf
+#sed -i "/#list server/i\    list server             '127.0.0.1#7874'" package/network/services/dnsmasq/files/dhcp.conf
+
+cp -rf ${GITHUB_WORKSPACE}/Applications/default-settings package/custom/default-settings
