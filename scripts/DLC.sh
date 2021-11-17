c08576a2be849a9787747c1c25dc7fef24079e38
Infsr <i@infsr.me>
2021-11-17 11:36:46 +0800

    lean

--- scripts/DLC.sh
+++ scripts/DLC.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 
-mkdir -p package/custom/
-cp -rf ${GITHUB_WORKSPACE}/Applications/luci-app-openclash package/custom/luci-app-openclash
+#mkdir -p package/custom/
+#cp -rf ${GITHUB_WORKSPACE}/Applications/luci-app-openclash package/custom/luci-app-openclash
 #get latest core
-bash ${GITHUB_WORKSPACE}/Modification/openclash/get_clash_core.sh amd64
+#bash ${GITHUB_WORKSPACE}/Modification/openclash/get_clash_core.sh amd64
 
 # dnsmasq forwarder
 #sed -i '/option resolvfile/i\      option noresolv         0' package/network/services/dnsmasq/files/dhcp.conf
 #sed -i 's/option resolvfile/#option resolvfile/g' package/network/services/dnsmasq/files/dhcp.conf
 #sed -i "/#list server/i\    list server             '127.0.0.1#7874'" package/network/services/dnsmasq/files/dhcp.conf
 
-cp -rf ${GITHUB_WORKSPACE}/Applications/default-settings package/custom/default-settings
+#cp -rf ${GITHUB_WORKSPACE}/Applications/default-settings package/custom/default-settings
+
+rm -rf package/lean/default-settings
+mv ${GITHUB_WORKSPACE}/community package/
--- scripts/Modification.sh
+++ scripts/Modification.sh
@@ -2,12 +2,12 @@
 
 
 ## add tools
-rm -rf tools/Makefile
-cp -rf ${GITHUB_WORKSPACE}/Modification/tools/* tools/
+#rm -rf tools/Makefile
+#cp -rf ${GITHUB_WORKSPACE}/Modification/tools/* tools/
 
 ## use O2 optimization
-sed -i 's/Os/O2/g' include/target.mk
-sed -i 's/O2/O2/g' rules.mk
+#sed -i 's/Os/O2/g' include/target.mk
+#sed -i 's/O2/O2/g' rules.mk
 
 
 ## remove show SNAPSHOT tags
@@ -17,11 +17,11 @@ sed -i 's/ %V,//g' package/base-files/files/etc/banner
 
 
 ## Modify the maximum number of Active Connections
-sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+#sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 
 ## Remove status show model
-sed -i '55d' feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+#sed -i '55d' feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
 
 ## TCP Fast Open(TFO)
@@ -31,9 +31,9 @@ cp -rf ${GITHUB_WORKSPACE}/Modification/base-files/etc/sysctl.d/60_tcp_fastopen.
 
 
 ## custom compile information
-sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-sed -i "$ a\DISTRIB_DESCRIPTION='Built by Infsr($(date +%Y.%m.%d))@%D %V %C'" package/base-files/files/etc/openwrt_release
-sed -i '/%D/a\ Infsr Build' package/base-files/files/etc/banner
+#sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+#sed -i "$ a\DISTRIB_DESCRIPTION='Built by Infsr($(date +%Y.%m.%d))@%D %V %C'" package/base-files/files/etc/openwrt_release
+#sed -i '/%D/a\ Infsr Build' package/base-files/files/etc/banner
 
 
 ## Crypto algorithm support kernel
