23f93d9b04facc32c86f33a5334bc86f083140d1
Infsr <i@infsr.me>
2021-11-18 01:06:04 +0800

    swap theme

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rm -rf package/feeds/luci/luci-app-wol
+rm -rf feeds/luci/applications/luci-app-wol
+rm -rf feeds/luci/modules/luci-newapi
+sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial_new/g' feeds/luci/collections/luci/Makefile
