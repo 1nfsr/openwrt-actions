f935087f9e3382d6f9e561ae6934184a4bbd34f5
Infsr <i@infsr.me>
2021-11-19 02:14:27 +0800

    fix ipv6

--- scripts/after.sh
+++ scripts/after.sh
@@ -3,4 +3,3 @@
 rm -rf feeds/luci/applications/luci-app-wol
 rm -rf feeds/luci/modules/luci-newapi
 sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial_new/g' feeds/luci/collections/luci/Makefile
-sed -i 's/odhcpd-ipv6only/odhcpd/g' package/community/lean/ipv6-helper/Makefile
