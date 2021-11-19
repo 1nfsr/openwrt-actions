3472a7f4518179018f285aaa52f46bcbc5679def
Infsr <i@infsr.me>
2021-11-19 11:33:55 +0800

    update

--- scripts/DLC.sh
+++ scripts/DLC.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 
+mkdir -p package/base-files/files/etc/openclash/
 #mkdir -p package/custom/
 #cp -rf ${GITHUB_WORKSPACE}/Applications/luci-app-openclash package/custom/luci-app-openclash
 #get latest core
 #bash ${GITHUB_WORKSPACE}/Modification/openclash/get_clash_core.sh amd64
+bash ${GITHUB_WORKSPACE}/scripts/get_clash_core.sh amd64
+cp -rf ${GITHUB_WORKSPACE}/diy/etc/openclash/* package/base-files/files/etc/openclash/
+cp -rf ${GITHUB_WORKSPACE}/diy/etc/config/openclash package/community/other/luci-app-openclash/root/etc/config/
 
 # dnsmasq forwarder
 #sed -i '/option resolvfile/i\      option noresolv         0' package/network/services/dnsmasq/files/dhcp.conf
@@ -12,3 +16,5 @@
 #sed -i "/#list server/i\    list server             '127.0.0.1#7874'" package/network/services/dnsmasq/files/dhcp.conf
 
 #cp -rf ${GITHUB_WORKSPACE}/Applications/default-settings package/custom/default-settings
+
+cp -rf ${GITHUB_WORKSPACE}/diy/etc/config/turboacc package/community/lean/luci-app-turboacc/root/etc/config/
