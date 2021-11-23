dbe4d95b5add5b55c1e73db98a9cd36d8f87c412
Infsr <i@infsr.me>
2021-11-23 10:27:39 +0800

    swap ls

--- diy/etc/startup_fullcone.sh
+++ diy/etc/startup_fullcone.sh
@@ -6,9 +6,10 @@ if [ "$fullcone_enable" -eq 0 ]; then
     echo "Enable full cone nat"
     uci -q set turboacc.config.fullcone_nat=1
     uci commit turboacc
-    sleep 3
-    echo "full cone nat Start Successful, Restart turboacc"
-    /etc/init.d/turboacc restart
+    #sleep 3
+    echo "full cone nat Start Successful"
+    #echo "Restart turboacc"
+    #/etc/init.d/turboacc restart
 else
     echo "full cone nat running"
 fi
