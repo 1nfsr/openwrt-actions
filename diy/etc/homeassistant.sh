6c4880ba5cbc10d9b85e54b7d453b1e53cf99434
Infsr <i@infsr.me>
2021-11-25 01:33:25 +0800

    fix

--- diy/etc/homeassistant.sh
+++ diy/etc/homeassistant.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # To Support Home Assistant
-sed -i 's/%v/18.06/g' >> /usr/lib/os-release
+#sed -i 's/%v/18.06/g'  /usr/lib/os-release
+sed -i 's/VERSION_ID=""/VERSION_ID="18.06"/g' /usr/lib/os-release
 
 sed -i 's/bash \/etc\/homeassistant.sh//g' /etc/rc.local
 rm $0
