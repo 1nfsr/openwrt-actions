a6d1821a1596d2f70c29ed5fecf91f0a7e9abf37
Infsr <i@infsr.me>
2022-01-03 23:46:00 +0800

    update

--- scripts/after.sh
+++ scripts/after.sh
@@ -3,3 +3,6 @@
 rm -rf feeds/luci/applications/luci-app-wol
 rm -rf feeds/luci/modules/luci-newapi
 sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial_new/g' feeds/luci/collections/luci/Makefile
+
+# mode health在新版haproxy已弃用
+sed -i 's/mode health/http-request return status 200/g' feeds/packages/net/haproxy/files/haproxy.cfg
--- scripts/before.sh
+++ scripts/before.sh
@@ -4,5 +4,3 @@
 rm -rf package/lean
 mv ${GITHUB_WORKSPACE}/community package/
 
-# mode health在新版haproxy已弃用
-sed -i 's/mode health/http-request return status 200/g' feeds/packages/net/haproxy/files/haproxy.cfg
